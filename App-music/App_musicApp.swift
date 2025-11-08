e3dccd65da6e9d70b7987343b067587def1b53cf
Jose Petrucio <jspetrucio@gmail.com>
2025-11-08 13:14:39 -0500

    Initial Commit

---
+++ App-music/App_musicApp.swift
@@ -0,0 +1,17 @@
+//
+//  App_musicApp.swift
+//  App-music
+//
+//  Created by josdasil on 11/8/25.
+//
+
+import SwiftUI
+
+@main
+struct App_musicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ App-music/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  App-music
+//
+//  Created by josdasil on 11/8/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
